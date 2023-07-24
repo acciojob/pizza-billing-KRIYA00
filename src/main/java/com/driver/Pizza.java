0751567daa85a09cd892b5dd609245471e5e3cae
ysi <yshouldi@gmail.com>
2023-07-24 14:47:21 +0530

    Pizza Billing-1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,33 +2,88 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    private int totalprice;
+    private int cheeseprice;
+    private int toppingprice;
+    private int paperbagprice;
+    private int baseprice;
     private Boolean isVeg;
     private String bill;
+    private Boolean ischeeseadded=false;
+    private  Boolean istoppingssadded=false;
+    private Boolean ispaperbagadded=false;
+    private Boolean isBillGenerated=false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+
         // your code goes here
+        if(isVeg==true)
+        {
+            baseprice = 300;
+            toppingprice=70;
+        }
+        else
+        {
+            baseprice=400;
+            toppingprice=120;
+        }
+        cheeseprice = 80;
+        paperbagprice=20;
+        totalprice=baseprice;
     }
 
     public int getPrice(){
-        return this.price;
+        return this.totalprice;
     }
 
-    public void addExtraCheese(){
+    public void addExtraCheese()
+    {
         // your code goes here
+        if(!ischeeseadded)
+        {
+            totalprice += cheeseprice;
+            ischeeseadded=true;
+        }
     }
 
-    public void addExtraToppings(){
+    public void addExtraToppings()
+    {
         // your code goes here
+        if(!istoppingssadded)
+        {
+            totalprice += toppingprice;
+            istoppingssadded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!ispaperbagadded)
+        {
+            totalprice+=paperbagprice;
+            ispaperbagadded=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(!isBillGenerated) {
+            isBillGenerated=true;
+            bill += "Base Price Of The Pizza: " + baseprice;
+            if (ischeeseadded)
+                bill += "Extra Cheese Added: " + cheeseprice;
+            if (istoppingssadded)
+                bill += "Extra Toppings Added: " + toppingprice;
+            if (ispaperbagadded)
+                bill += "Paperbag Added: " + paperbagprice;
+        }
+
+       /* Base Price Of The Pizza: 300
+        Extra Cheese Added: 80
+        Extra Toppings Added: 70
+        Paperbag Added: 20
+        Total Price:*/
         return this.bill;
     }
 }
